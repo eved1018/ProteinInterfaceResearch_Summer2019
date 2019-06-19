afd5504694abce489b3a8e73493f05f02dc6078e
E_Edelstein <edelsteinevan@gmail.com>
2019-06-19 17:03:46 -0400

    python script

---
+++ Raji_Summer2019_atom/Code/F_Score_Python.py
@@ -0,0 +1,62 @@
+#!/bin/python
+import os
+import sys
+
+
+
+def Fscore():
+    file_list = (sys.argv)
+    fileA = file_list[1]
+    fileB = file_list[2]
+    fileid = file_list[3]
+    my_set1= set()
+    my_set2 = set()
+    head = 0
+    with open(fileA, 'r+') as file1:
+
+            for line in file1:
+                sentence = line.replace('\t'," ")
+                sentence = sentence.replace('\n',"")
+                my_set1.add(sentence)
+                head += 1
+
+
+            with open(fileB, 'r+') as file2:
+                firstNlines = file2.readlines()[0:head]
+
+                for line2 in firstNlines:
+                    sentence2 = line2.replace('\n',"")
+                    my_set2.add(sentence2)
+
+
+            known_residues = len(my_set1)
+            print(known_residues , "experimental/known interface residues")
+            print(my_set1)
+
+            predicted_residues = len(my_set2)
+            print(predicted_residues, "predicted interface resiues")
+            print(my_set2)
+
+            True_Positives = len(my_set1 & my_set2)
+            print(True_Positives,"correctly predicted residues")
+            print(my_set1 & my_set2)
+
+
+            False_positives = len(my_set2 - my_set1)
+            print(False_positives, "False positives")
+            print(my_set2 - my_set1)
+
+
+            F_score = True_Positives / known_residues
+            print("F_score = ", F_score)
+
+
+            with open('/Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/Predus/F_scores.txt', 'a+') as file3:
+                file3.write(fileid)
+                file3.write("    ")
+                file3.write(str(F_score))
+                file3.write("\n")
+
+
+
+Fscore()

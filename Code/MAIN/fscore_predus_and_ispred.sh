cbbd50356306ac9ac05e4171927cac2c9651efcf
E_Edelstein <edelsteinevan@gmail.com>
2019-07-24 18:22:46 -0400

    com

--- Code/MAIN/fscore_predus_and_ispred.sh
+++ Code/MAIN/fscore_predus_and_ispred.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 
-echo "protien Predus Ispred Vorffip" > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt
+echo "protien Predus Ispred" > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt
 File=/Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/Fscores/*
-paste $File | grep '^1.*$'| awk '{if ($1==$6 && $1==$11) print $1, $5, $10, $15; else print $1}'  >> /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt
+echo $File
+paste $File | grep '^1.*$'| awk '{if ($1==$2 && $1==$7) print $1, $6, $11 ; else print $1}'  >> /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt
 f=`grep -E -i  "global" /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/Fscores/* | awk -F: '{print $2}' | awk -F= 'BEGIN { ORS="\t" }; { print  $2 }' | awk -F% '{print $1}' `
 A=`grep -E -i  "Average" /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/Fscores/* | awk -F: '{print $2}' | awk -F= 'BEGIN { ORS="\t" }; { print  $2 }' | awk -F% '{print $1}'`
 echo "GFS" ${f} >>/Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt
 echo "AFS" ${A} >>/Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt
-cat /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt | column -t > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/F_Score_results_full
+cat /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/tmp.txt | column -t > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/Fscores/F_Score_results_full
---
+++ Code/MAIN/unsortedscores.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+for f in /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/Predus/predus_outputfiles/*
+do
+  protienID=`echo $f | awk -F/ '{print toupper $11}' | awk -F. '{print $2}' | sed 's/\_/./g'`
+  cat $f | awk '{print $6, $11}' > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/Predus/unsorted/${protienID}_Porder
+
+done

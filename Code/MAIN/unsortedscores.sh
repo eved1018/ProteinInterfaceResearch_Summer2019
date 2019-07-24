602ed1fe1c38d4db362af0335345bb7abec63e96
E_Edelstein <edelsteinevan@gmail.com>
2019-07-24 19:02:55 -0400

    c

--- Code/MAIN/unsortedscores.sh
+++ Code/MAIN/unsortedscores.sh
@@ -4,6 +4,6 @@
 for f in /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/Predus/predus_outputfiles/*
 do
   protienID=`echo $f | awk -F/ '{print toupper $11}' | awk -F. '{print $2}' | sed 's/\_/./g'`
-  cat $f | awk '{print $6, $11, $11 * ".5212705531" , $11 * "0.493834223" }'| column -t > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/Predus/unsorted/${protienID}_Porder
-
+  cat $f | awk '{print $6, $11, $11 * ".5212705531" , $11 * "0.493834223" }'| uniq | column -t > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/coeffeciant/${protienID}_PCOEF
+  
 done

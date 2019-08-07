d27af13aae17e34ba654bed8205d79aeaf4f96f2
E_Edelstein <edelsteinevan@gmail.com>
2019-08-06 22:47:01 -0400

    c

--- Code/MAIN/unsortedscores.sh
+++ Code/MAIN/unsortedscores.sh
@@ -3,7 +3,9 @@
 
 for f in /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/Predus/predus_outputfiles/*
 do
+  coefA=".5212705531"
+  coefB="0.493834223"
   protienID=`echo $f | awk -F/ '{print toupper $11}' | awk -F. '{print $2}' | sed 's/\_/./g'`
-  cat $f | awk '{print $6, $11, $11 * ".5212705531" , $11 * "0.493834223" }'| uniq | column -t > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/coeffeciant/${protienID}_PCOEF
-  
+  cat $f | awk -v A=$coefA B=$coefB '{print $6, $11, $11 * A , $11 * B }'| uniq | column -t > /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/coeffeciant/${protienID}_PCOEF
+
 done
--- Code/called_scripts/column.sh
+++ Code/called_scripts/column.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-for file in /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/ROC_scores/Pscores/*
+for file in /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/ROCcurve/*
 do
-  echo $file >> /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/ROC_scores/rocscores.txt
-  column -t $file >> /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/Data_files/ROC_scores/rocscores.txt
+  echo $file >> /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/ROCtemp.txt
+  column -t $file >> /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/CoefficantROC.txt
 done
+
+rm /Users/evanedelstein/Desktop/LAB/Raji_Summer2019_atom/Code/results/CoeffeciantROC/ROCtemp.txt

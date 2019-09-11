84dc4deaa0639ddb372e9799e31a8751399d281b
E_Edelstein <edelsteinevan@gmail.com>
2019-09-11 17:46:58 -0400

    c

--- Scripts/Other:extra/PDB_Parser.R
+++ Scripts/Other:extra/PDB_Parser.R
@@ -1,7 +1,7 @@
 #!/bin/R
 # make itrative variabel for protien id's
 # make sure system taeks in path to testquery and path for output
-
+#ids must be splt but _ not . 
 library(bio3d)
-id <- system("cat Data_files/testquery30" , intern= TRUE )
-pdbsplit( get.pdb( c(id), URLonly=TRUE , path = "/Data_files/Predus/Predus_inputfiles/") )
+id <- "2hmg_pdb"
+pdbsplit( get.pdb( c(id), URLonly=TRUE , path = "~/Desktop") )

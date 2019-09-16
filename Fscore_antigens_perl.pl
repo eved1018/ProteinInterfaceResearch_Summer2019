d2ee05c76ab8af9690987db8f576ac3fa42565fb
stevenabijmil <abijmil@mail.yu.edu>
2019-09-15 21:05:12 -0400

    c

---
+++ Fscore_antigens_perl.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+#declaration of file names as elements in an array
+my $preddir = <//>;
+my $Dbmark_annotateddir = </Users/mordechaiwalder/Desktop/Research_Mordechai/Annotated_Residues/Dbmark_Annotated_Residues>;

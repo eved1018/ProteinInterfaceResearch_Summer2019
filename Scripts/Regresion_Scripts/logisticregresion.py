ef790a511efdce11322e9f27d419668ab63d1d60
E_Edelstein <edelsteinevan@gmail.com>
2020-03-09 13:41:13 -0400

    commit

--- Scripts/Regresion_Scripts/logisticregresion.py
+++ Scripts/Regresion_Scripts/logisticregresion.py
@@ -22,17 +22,10 @@ def log_reg_nox():
     protein = data.residue
     X = data[feature_cols] # Features
     y = data.annotated # Target variable
-    # X_train,X_test,y_train,y_test=train_test_split(X,y)
-    # instantiate the model (using the default parameters)
-    # logreg = LogisticRegression()
-    # fit the model with data
-    # logreg.fit(X,y)
     x = sm.add_constant(X)
     logit_model=sm.Logit(y,x)
     result=logit_model.fit()
     print(result.summary2())
-    # print(logreg.coef_)
-    # print(logreg.intercept_)
     coefficients = result.params
     benchmark= pd.read_csv('/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/benchmarkdata.csv', header=None, names=col_names)
     protein= benchmark.residue
@@ -101,21 +94,13 @@ def log_reg_nox_ispred_dockpred():
     protein = data.residue
     X = data[feature_cols] # Features
     y = data.annotated # Target variable
-    # X_train,X_test,y_train,y_test=train_test_split(X,y)
-    # instantiate the model (using the default parameters)
-    # logreg = LogisticRegression()
-    # fit the model with data
-    # logreg.fit(X,y)
     x = sm.add_constant(X)
     logit_model=sm.Logit(y,x)
     result=logit_model.fit()
     print(result.summary2())
-    # print(logreg.coef_)
-    # print(logreg.intercept_)
     coefficients = result.params
     benchmark= pd.read_csv('/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/benchmarkdata.csv', header=None, names=col_names)
     protein= benchmark.residue
-    predusval = benchmark.predus
     ispredval = benchmark.ispred
     dockpredval = benchmark.dockpred
     ispredcoef = coefficients[1]
@@ -139,12 +124,6 @@ def log_reg_bnch_ispred_dockpred():
     protein = data.residue
     X = data[feature_cols] # Features
     y = data.annotated # Target variable
-    # X_train,X_test,y_train,y_test=train_test_split(X,y)
-    # instantiate the model (using the default parameters)
-    # logreg = LogisticRegression()
-    # fit the model with data
-    # logreg.fit(X,y)
-    # import statsmodels.api as sm
     x = sm.add_constant(X)
     logit_model=sm.Logit(y,x)
     result=logit_model.fit()
@@ -155,7 +134,6 @@ def log_reg_bnch_ispred_dockpred():
     nox= pd.read_csv('/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/noxdata.csv', header=None, names=col_names)
     protein= nox.residue
     protein= nox.residue
-    predusval = nox.predus
     ispredval = nox.ispred
     dockpredval = nox.dockpred
     ispredcoef = coefficients[1]
@@ -178,22 +156,14 @@ def log_reg_nox_predus_dockpred():
     protein = data.residue
     X = data[feature_cols] # Features
     y = data.annotated # Target variable
-    # X_train,X_test,y_train,y_test=train_test_split(X,y)
-    # instantiate the model (using the default parameters)
-    # logreg = LogisticRegression()
-    # fit the model with data
-    # logreg.fit(X,y)
     x = sm.add_constant(X)
     logit_model=sm.Logit(y,x)
     result=logit_model.fit()
     print(result.summary2())
-    # print(logreg.coef_)
-    # print(logreg.intercept_)
     coefficients = result.params
     benchmark= pd.read_csv('/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/benchmarkdata.csv', header=None, names=col_names)
     protein= benchmark.residue
     predusval = benchmark.predus
-    ispredval = benchmark.ispred
     dockpred = benchmark.dockpred
     preduscoef = coefficients[1]
     dockpredcoef = coefficients[2]
@@ -216,12 +186,66 @@ def log_reg_bnch__predus_dockpred():
     protein = data.residue
     X = data[feature_cols] # Features
     y = data.annotated # Target variable
-    # X_train,X_test,y_train,y_test=train_test_split(X,y)
-    # instantiate the model (using the default parameters)
-    # logreg = LogisticRegression()
-    # fit the model with data
-    # logreg.fit(X,y)
-    # import statsmodels.api as sm
+    x = sm.add_constant(X)
+    logit_model=sm.Logit(y,x)
+    result=logit_model.fit()
+    print(result.summary2())
+    coefficients = result.params
+    print(coefficients)
+    # prediction value 
+    nox= pd.read_csv('/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/noxdata.csv', header=None, names=col_names)
+    protein= nox.residue
+    protein= nox.residue
+    predusval = nox.predus
+    dockpredval = nox.dockpred
+    preduscoef = coefficients[1]
+    dockpredcoef = coefficients[2]
+    val = (coefficients[0] + preduscoef * predusval +  dockpredval*  dockpredcoef)*(-1)
+    exponent = np.exp(val)
+    pval = (1/(1+exponent))
+    results = pd.DataFrame({"residue": protein, "prediction value": pval})
+    path="/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/predictionvalues/predus_dockpred/noxpredictionvalues_predus_dockpred.csv"
+    results.to_csv(path,sep=",", index=False, header=True)
+log_reg_bnch__predus_dockpred()
+def log_reg_nox_predus_ispred():
+    col_names = ['residue', 'predus', 'ispred', 'dockpred', 'annotated']
+    # load dataset
+    df = pd.read_csv("/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/noxdata.csv", header=None, names=col_names)
+    # print(df.head())
+    df.isnull().any()
+    data = df.fillna(method='ffill')
+    feature_cols = ['predus','ispred']
+    protein = data.residue
+    X = data[feature_cols] # Features
+    y = data.annotated # Target variable
+    x = sm.add_constant(X)
+    logit_model=sm.Logit(y,x)
+    result=logit_model.fit()
+    print(result.summary2())
+    coefficients = result.params
+    benchmark= pd.read_csv('/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/benchmarkdata.csv', header=None, names=col_names)
+    protein= benchmark.residue
+    predusval = benchmark.predus
+    ispredval = benchmark.ispred
+    preduscoef = coefficients[1]
+    ispredcoef = coefficients[2]
+    val = (coefficients[0] + preduscoef * predusval + ispredval* ispredcoef)*(-1)
+    exponent = np.exp(val)
+    pval = (1/(1+exponent))
+    results = pd.DataFrame({"residue": protein, "prediction value": pval})
+    path="/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/predictionvalues/predus_ispred/benchmarkpredictionvalues_predus_ispred.csv"
+    results.to_csv(path,sep=",", index=False, header=True)
+log_reg_nox_predus_ispred()
+def log_reg_bnch__predus_ispred():
+    col_names = ['residue', 'predus', 'ispred', 'dockpred', 'annotated']
+    # load dataset
+    df = pd.read_csv("/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/benchmarkdata.csv", header=None, names=col_names)
+    df.isnull().any()
+    data = df.fillna(method='ffill')
+    feature_cols = ['predus','ispred']
+    protein = data.residue
+    X = data[feature_cols] # Features
+    y = data.annotated # Target variable
     x = sm.add_constant(X)
     logit_model=sm.Logit(y,x)
     result=logit_model.fit()
@@ -234,13 +258,12 @@ def log_reg_bnch__predus_dockpred():
     protein= nox.residue
     predusval = nox.predus
     ispredval = nox.ispred
-    dockpred = nox.dockpred
-    predcoef = coefficients[1]
+    preduscoef = coefficients[1]
     ispredcoef = coefficients[2]
-    val = (coefficients[0] + predcoef * predusval +  dockpred* predcoef)*(-1)
+    val = (coefficients[0] + preduscoef * predusval +  ispredval*  ispredcoef)*(-1)
     exponent = np.exp(val)
     pval = (1/(1+exponent))
     results = pd.DataFrame({"residue": protein, "prediction value": pval})
-    path="/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/predictionvalues/predus_dockpred/noxpredictionvalues_predus_dockpred.csv"
+    path="/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Logistic_regresion_corrected/predictionvalues/predus_ispred/noxpredictionvalues_predus_ispred.csv"
     results.to_csv(path,sep=",", index=False, header=True)
-log_reg_bnch__predus_dockpred()
+log_reg_bnch__predus_ispred()
